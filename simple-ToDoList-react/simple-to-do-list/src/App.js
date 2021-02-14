89b1cc69cfdd4177c4e909b28a019bad6d4b627e
Diogo Souza <digsuk360@gmail.com>
2021-02-14 01:33:33 -0300

    Tentativa_1

--- simple-ToDoList-react/simple-to-do-list/src/App.js
+++ simple-ToDoList-react/simple-to-do-list/src/App.js
@@ -1,9 +1,47 @@
-import React from 'react';
-import './App.css';
-import Header from './components';
+/* eslint-disable no-shadow */
+import React, { useState } from 'react';
+import {
 
-const Routes = () => (
-  <Header title="Pitang 2" />
-);
+} from 'react-bootstrap';
 
-export default Routes;
+const App = () => {
+  const [todos, setTodos] = useState([{
+    isCompleted: false,
+  }]);
+  const [text, setText] = useState('');
+
+  const checkComplete = (id) => {
+    const newTodos = [...todos];
+    newTodos[id].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  return (
+    <div>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          setTodos([...todos, { title: text }]);
+          setText('');
+        }}
+      >
+        <input
+          type="text"
+          value={text}
+          onChange={(event) => setText(event.target.value)}
+        />
+        <button type="submit">adicionar</button>
+      </form>
+      <ul>
+        {todos.map((todo, index /*  checkComplete */) => (
+          <li>
+            <input className="m-2" type="checkbox" onClick={() => checkComplete(index)} style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }} />
+            {todo.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
