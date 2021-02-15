a630b0fc606e1ad4fdc3b8dd6c1989b3680d1345
Diogo Souza <digsuk360@gmail.com>
2021-02-14 21:03:42 -0300

    2-check if task was completed-done-withCard

--- simple-ToDoList-react/simple-to-do-list/src/App.js
+++ simple-ToDoList-react/simple-to-do-list/src/App.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-shadow */
 import React, { useState } from 'react';
 import {
-
+  Container, Row, Col, Form, Button,
 } from 'react-bootstrap';
+
+import Card from './components/Card';
 
 const App = () => {
   const [todos, setTodos] = useState([{
@@ -16,38 +18,58 @@ const App = () => {
     setTodos(newTodos);
   };
 
+  const submitButtonAdd = () => {
+    if (!text.trim()) {
+      alert('Ta vazio');
+      return;
+    }
+    setTodos([...todos, { title: text }]);
+    setText('');
+  };
+
   return (
-    <div>
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-          setTodos([...todos, { title: text }]);
-          setText('');
-        }}
-      >
-        <input
-          type="text"
-          value={text}
-          onChange={(event) => setText(event.target.value)}
-        />
-        <button type="submit">adicionar</button>
-      </form>
-      <ul>
-        {todos.map((todo, index) => (
-          <li
-            key={todo.title}
-            style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }}
-          >
-            <input
-              className="m-2"
-              type="checkbox"
-              onClick={() => checkComplete(index)}
-            />
-            {todo.title}
-          </li>
-        ))}
-      </ul>
-    </div>
+
+    <Card title="Todo App" className="m-4">
+      <Container>
+        <h2>Lista de atividades</h2>
+        <Row>
+          <Col xl={12} md={9}>
+            <Form>
+              <Form.Group>
+                <Form.Control
+                  type="text"
+                  value={text}
+                  onChange={(event) => setText(event.target.value)}
+                  placeholder="Insira sua atividade do dia"
+                />
+              </Form.Group>
+            </Form>
+          </Col>
+          <Col>
+            <Button disabled={!text.trim()} onClick={submitButtonAdd} type="button">
+              Adicionar Todo
+            </Button>
+          </Col>
+        </Row>
+        <Row>
+          <ul>
+            {todos.map((todo, index) => (
+              <li
+                key={todo.title}
+                style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }}
+              >
+                <input
+                  className="m-2"
+                  type="checkbox"
+                  onClick={() => checkComplete(index)}
+                />
+                {todo.title}
+              </li>
+            ))}
+          </ul>
+        </Row>
+      </Container>
+    </Card>
   );
 };
 
